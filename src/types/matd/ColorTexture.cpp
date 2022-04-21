61a02f0305bc06634cb6c0c89d6837eac20e6300
lazzy07 <222lasantha@gmail.com>
2022-04-21 09:55:14 +0530

    Texture functionality changes

--- src/types/matd/ColorTexture.cpp
+++ src/types/matd/ColorTexture.cpp
@@ -3,3 +3,8 @@
 MATD::ColorTexture::ColorTexture(const MATD_TEXTURE_BIT_TYPE bitType, const size_t width, const size_t height, const buf_type argType) : Texture(MAT_ARG::MAT_TEXCOLOR, bitType, 3,width, height, argType)
 {
 }
+
+MATD::ColorTexture::~ColorTexture()
+{
+	MATD_CORE_TRACE("MATD::TEX ColorTexture deleted");
+}
--- src/types/matd/GrayscaleTexture.cpp
+++ src/types/matd/GrayscaleTexture.cpp
@@ -3,3 +3,8 @@
 MATD::GrayscaleTexture::GrayscaleTexture(const MATD_TEXTURE_BIT_TYPE bitType, const size_t width, const size_t height, const buf_type argType) : Texture(MAT_ARG::MAT_TEXGRAYSCALE, bitType, 1, width, height, argType)
 {
 }
+
+MATD::GrayscaleTexture::~GrayscaleTexture()
+{
+	MATD_CORE_TRACE("MATD::TEX GrayscaleTexture deleted");
+}
